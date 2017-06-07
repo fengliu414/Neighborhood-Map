98c346332894ed163c7fd662a37c442bafd1cdff
liu feng <Feng@lius-MacBook-Pro.local>
2017-06-07 09:33:55 -0700

    final touch

--- js/app.js
+++ js/app.js
@@ -18,6 +18,22 @@ var foursquareSecret ='0W3NQ5CHYEAT3ID4BSYNBKLGAPF1OUNWONXQ1VXGJ0NNGMAS';
 var foursquareVersion = '20170112';
 var ajaxContents = new Array();
 
+$( ".cross" ).hide();
+$( ".options-box" ).hide();
+$( ".hamburger" ).click(function() {
+  $( ".options-box" ).slideToggle( "slow", function() {
+    $( ".hamburger" ).hide();
+    $( ".cross" ).show();
+  });
+});
+
+$( ".cross" ).click(function() {
+  $( ".options-box" ).slideToggle( "slow", function() {
+    $( ".cross" ).hide();
+    $( ".hamburger" ).show();
+  });
+});
+
 function getAjaxContent() {
   for (var i = 0; i < locations.length; i++) {
     // Get the position from the location array.
@@ -39,35 +55,6 @@ function getAjaxContent() {
     });
   }
 }
-
-// These are the real estate listings that will be shown to the user.
-// Normally we'd have these in a database instead.
-var locations = [
-  {title: 'Red Rock Coffee', location: {lat: 37.393605, lng: -122.078757}, placeID: 'ChIJF8VqETS3j4ARxmLCuGE7dzk',
-   id: '0'},
-  {title: "Starbucks", location: {lat: 37.421811, lng: -122.096427}, placeID: 'ChIJgRfYFxC6j4ARIlJOMpxVRng',
-  id: '1'},
-  {title: 'Pearl Cafe', location: {lat: 37.402987, lng: -122.106671}, placeID: 'ChIJb6azp5iwj4ARJkf05cvTjbU',
-   id: '2'},
-  {title: 'Coffee Lab', location: {lat: 37.417963, lng: -122.07105}, placeID: 'ChIJZ8G4P1i3j4AROfHcuW0v50M',
-   id: '3'},
-  {title: 'Starbucks', location: {lat: 37.411317, lng: -122.094452}, placeID: 'ChIJW5IcPqiwj4AR3dux-nE8hG8',
-   id: '4'},
-  {title: 'Moksha Coffee Roasting LLC', location: {lat: 37.413786, lng: -122.088661},
-   placeID: 'ChIJ59zoCKqwj4ARiPBNJAV9H5Q', id: '5'},
-  {title: 'Starbucks', location: {lat: 37.415999, lng: -122.077131}, placeID: 'ChIJ0Ttxbla3j4AROkXAJNLP9bw',
-   id: '6'},
-  {title: 'Starbucks', location: {lat: 37.402724, lng: -122.112076}, placeID: 'ChIJJRCUp56wj4AR3VdlpTrpOco',
-   id: '7'},
-  {title: 'Starbucks', location: {lat: 37.402898, lng: -122.079455}, placeID: 'ChIJA-tWDUy3j4ARr-ooF1e_rHk',
-   id: '8'},
-  {title: "Peet's Coffee", location: {lat: 37.400653, lng: -122.113264}, placeID: 'ChIJK0H565uwj4ARb86UbwMJu-o',
-   id: '9'},
-  {title: "Peet's Coffee", location: {lat: 37.419108, lng: -122.110495}, placeID: 'ChIJ_XZttG26j4AR5fLRimOa0x4',
-   id: '10'},
-  {title: 'Verde Tea Cafe', location: {lat: 37.394164, lng: -122.079489}, placeID: 'ChIJmVifQDS3j4AR6M7kW4ikBTw',
-   id: '11'}
-];
 
 // maps loading error handling
 function mapError() {
@@ -303,11 +290,11 @@ function searchWithinFilter() {
   for (var i = 0; i < markers.length; i++) {
     var filter = window.vm.filter();
     if (markers[i].title.toLowerCase().indexOf(filter.toLowerCase()) != -1) {
-      markers[i].setMap(map);
+      markers[i].setVisible(true);
       bounds.extend(markers[i].position);
       atleastOne = true;
     } else {
-      markers[i].setMap(null);
+      markers[i].setVisible(false);
     }
   }
   if (atleastOne) {
